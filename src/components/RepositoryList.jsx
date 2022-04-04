fb3d470677e328e51f8077a2aeef5e6e3c0d299a
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-04 23:25:44 +0100

    First component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+export const RepositoryList = () => {
+    return (
+        <section>
+            <h1>Repository List</h1>
+
+            <ul>
+                <li>
+                    <string>unform</string>
+                    <p>Forms in React</p>
+
+                    <a href="">Acess repository</a>
+                </li>
+                <li>
+                    <string>unform</string>
+                    <p>Forms in React</p>
+
+                    <a href="">Acess repository</a>
+                </li>
+                <li>
+                    <string>unform</string>
+                    <p>Forms in React</p>
+
+                    <a href="">Acess repository</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
