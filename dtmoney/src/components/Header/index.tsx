ba40639d9530118c8275a43aafece345aa244da1
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-05 14:55:53 +0100

    add(dtmoney): Header

---
+++ dtmoney/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logoImg from "../../assets/logo.svg"
+import { Container, Content } from "./styles"
+
+export const Header = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
