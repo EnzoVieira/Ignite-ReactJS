a7dfc71e086f8f63b1b8602f6b08533e1b2c6272
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-07 22:52:55 +0100

    (ignews): Prevent duplicating customers in stripe

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from "next-auth/react"
+
+import { api } from "../../services/api"
+import { getStripeJs } from "../../services/stripe-js"
+
 import styles from "./styles.module.scss"
 
 interface ISubscribeButtonProps {
@@ -5,8 +10,33 @@ interface ISubscribeButtonProps {
 }
 
 export const SubscribeButton = ({ priceId }: ISubscribeButtonProps) => {
+  const { data: session } = useSession()
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn("github")
+      return
+    }
+
+    try {
+      const response = await api.post("/subscribe")
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button className={styles.subscribeButton} type="button">
+    <button
+      className={styles.subscribeButton}
+      type="button"
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   )
