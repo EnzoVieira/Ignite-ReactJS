cd8001109d4f97654fca5fa9f6584ec11a3aff89
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-07 14:54:04 +0100

    add(ignews): SubscribeButton

---
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss"
+
+export const SubscribeButton = () => {
+  return (
+    <button className={styles.subscribeButton} type="button">
+      Subscribe now
+    </button>
+  )
+}
