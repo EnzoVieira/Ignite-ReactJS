24d1c45ea6e1dd4c501085ce511a80155ab47f3a
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-07 14:49:50 +0100

    add(ignews): Home page

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 
+import styles from "./home.module.scss"
+
 export default function Home() {
   return (
     <>
@@ -7,7 +9,22 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
