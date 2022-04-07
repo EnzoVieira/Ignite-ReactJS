64d9cf1745b5c67d2d4f31c2865852b5ea8596b6
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-07 14:11:40 +0100

    (ignews): Project setup

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <h1>Hello world</h1>
+    </>
+  )
+}
